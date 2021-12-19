eb334b028f2c99eb4d528cf950551105d1cf4240
Ao Li <leeleo3x@gmail.com>
2021-12-19 14:31:13 -0600

    update.

--- scripts/experiments/data-processing/process_data.py
+++ scripts/experiments/data-processing/process_data.py
@@ -4,7 +4,6 @@
 import sys
 import os
 import pandas as pd
-from glob import glob
 from visualize import process_data, generate_valid_cov_fig
 
 
@@ -13,19 +12,22 @@ DATASET = {"ant", "maven", "bcel", "rhino", "closure"}
 ALGORITHM = {"zest", "ei"}
 
 def generate(base_path: str):
-    for idx in range(0, 10):
-        for dataset in DATASET:
-            results = []
-            for algorithm in ALGORITHM:
+    for dataset in DATASET:
+        results = []
+        for algorithm in ALGORITHM:
+            for idx in range(0, 10):
                 path = os.path.join(base_path, f"{dataset}-{algorithm}-results-{idx}")
                 if not os.path.exists(path):
-                    return
+                    break
                 print(f"processing: {os.path.basename(path)}")
                 data = process_data(path)
                 results.append(data)
                 generate_valid_cov_fig(os.path.join(path, "valid_cov.pdf"), data)
-            merged_data = pd.concat(results, ignore_index=True, sort=False)
-            generate_valid_cov_fig(os.path.join(base_path, "figs", f"{dataset}-{idx}-valid_cov.pdf"), merged_data)
+        if not results:
+            continue
+        merged_data = pd.concat(results, ignore_index=True, sort=False)
+        generate_valid_cov_fig(os.path.join(base_path, "figs",
+                                            f"{dataset}-valid_cov.pdf"), merged_data)
 
 
 def main():
