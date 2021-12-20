fa7e3c97bfa3a2b7332fe7c19b6c9bf7316024f4
Ao Li <leeleo3x@gmail.com>
2021-12-20 11:05:20 -0600

    Fix visualization script.

--- scripts/experiments/data-processing/process_data.py
+++ scripts/experiments/data-processing/process_data.py
@@ -15,18 +15,24 @@ def generate(base_path: str):
     for dataset in DATASET:
         results = []
         for algorithm in ALGORITHM:
+            algo_results = []
             for idx in range(0, 10):
                 path = os.path.join(base_path, f"{dataset}-{algorithm}-results-{idx}")
                 if not os.path.exists(path):
                     break
                 print(f"processing: {os.path.basename(path)}")
                 data = process_data(path)
-                results.append(data)
-                generate_valid_cov_fig(os.path.join(path, "valid_cov.pdf"), data)
+                algo_results.append(data)
+                generate_valid_cov_fig(os.path.join(path, "valid_cov.pdf"), data, data.shape[0] // 1000)
+            min_length = min([d.shape[0] for d in algo_results])
+            results.extend([d[:min_length:min_length // 1000] for d in algo_results])
         if not results:
             continue
+        out_folder = os.path.join(base_path, "figs")
+        if not os.path.exists(out_folder):
+            os.mkdir(out_folder)
         merged_data = pd.concat(results, ignore_index=True, sort=False)
-        generate_valid_cov_fig(os.path.join(base_path, "figs",
+        generate_valid_cov_fig(os.path.join(out_folder,
                                             f"{dataset}-valid_cov.pdf"), merged_data)
 
 
--- scripts/experiments/data-processing/visualize.py
+++ scripts/experiments/data-processing/visualize.py
@@ -11,16 +11,16 @@ def process_data(path: str) -> pd.DataFrame:
     data = pd.read_csv(os.path.join(path, "plot_data"), sep=",", skipinitialspace=True,
                        converters={"valid_cov": p2f, "map_size": p2f})
     data['# unix_time'] -= data["# unix_time"][0]
-
-    data = data[::100] # downsampling
-
+    data['total_inputs'] = data['valid_inputs'] + data['invalid_inputs']
+    data = data.set_index("total_inputs").reindex(
+        range(1, data.total_inputs.max())).interpolate().reset_index()
     algorithm = os.path.basename(path).split('-')[1]
     data['algorithm'] = [algorithm] * data.shape[0]
-    data['total_inputs'] = data['valid_inputs'] + data['invalid_inputs']
     return data
 
-def generate_valid_cov_fig(path: str, data: pd.DataFrame):
-    axis = sns.lineplot(x="total_inputs", y="valid_cov", hue='algorithm', 
+def generate_valid_cov_fig(path: str, data: pd.DataFrame, step=1):
+    data = data[::step]
+    axis = sns.lineplot(x="total_inputs", y="valid_cov", hue='algorithm',
                         hue_order=sorted(data['algorithm'].unique()), data=data)
     fig = axis.get_figure()
     fig.savefig(path)
