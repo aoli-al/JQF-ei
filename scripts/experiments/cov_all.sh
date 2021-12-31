ee57b0f94b1fa2c9e8ed0f219c0fb77b0d0b1605
Ao Li <leeleo3x@gmail.com>
2021-12-31 16:55:30 -0600

    Fix experiment scripts.

--- scripts/experiments/cov_all.sh
+++ scripts/experiments/cov_all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-./cov.sh chess chess.FENTest $1
-./cov.sh ant ant.ProjectBuilderTest $1
-./cov.sh maven maven.ModelReaderTest $1
-./cov.sh bcel bcel.ParserTest $1
-./cov.sh closure closure.CompilerTest $1
-./cov.sh rhino rhino.CompilerTest $1
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+# $SCRIPT_DIR/cov.sh chess chess.FENTest $1
+$SCRIPT_DIR/cov.sh ant ant.ProjectBuilderTest $1
+$SCRIPT_DIR/cov.sh maven maven.ModelReaderTest $1
+$SCRIPT_DIR/cov.sh bcel bcel.ParserTest $1
+$SCRIPT_DIR/cov.sh closure closure.CompilerTest $1
+$SCRIPT_DIR/cov.sh rhino rhino.CompilerTest $1
--- scripts/experiments/run_all.sh
+++ scripts/experiments/run_all.sh
@@ -7,26 +7,34 @@ fi
 
 id1=$1
 id2=$2
-time=3h
+time=$((3*60*60))
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for id in $(seq $id1 $id2); do
 
-  ./bench.sh ant ant.ProjectBuilderTest $id $time ant-project-afl.dict xml/build.xml
-  sleep $time 10s
+  $SCRIPT_DIR/bench.sh ant ant.ProjectBuilderTest $id $time ant-project-afl.dict xml/build.xml
+  sleep $time 
+  sleep 10
 
-  ./bench.sh maven maven.ModelReaderTest $id $time maven-model-afl.dict xml/pom.xml
-  sleep $time 10s
+  $SCRIPT_DIR/bench.sh maven maven.ModelReaderTest $id $time maven-model-afl.dict xml/pom.xml
+  sleep $time 
+  sleep 10
 
-  ./bench.sh bcel bcel.ParserTest $id $time javaclass.dict javaclass/Hello.class
-  sleep $time 10s
+  $SCRIPT_DIR/bench.sh bcel bcel.ParserTest $id $time javaclass.dict javaclass/Hello.class
+  sleep $time 
+  sleep 10
 
-  ./bench.sh closure closure.CompilerTest $id $time javascript.dict js/react.production.min.js
-  sleep $time 10s
+  $SCRIPT_DIR/bench.sh closure closure.CompilerTest $id $time javascript.dict js/react.production.min.js
+  sleep $time 
+  sleep 10
 
-  ./bench.sh rhino rhino.CompilerTest $id $time javascript.dict js/react.production.min.js
-  sleep $time 10s
+  $SCRIPT_DIR/bench.sh rhino rhino.CompilerTest $id $time javascript.dict js/react.production.min.js
+  sleep $time 
+  sleep 10
 
   ./bench.sh chess chess.FENTest $id $time fen.dict fen/initial.fen
-  sleep $time 10s
+  sleep $time 
+  sleep 10
 
 done
