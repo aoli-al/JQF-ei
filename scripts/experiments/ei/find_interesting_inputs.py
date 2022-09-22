db08bbbade81e3f3f08874c03086c347e5011b63
Ao Li <leeleo3x@gmail.com>
2022-09-22 15:03:39 -0400

    Update data processing scripts.

--- scripts/experiments/ei/find_interesting_inputs.py
+++ scripts/experiments/ei/find_interesting_inputs.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 from configs import *
 from visualize import *
 import re
@@ -13,7 +14,7 @@ def build_corpus_map(path: str) -> Dict[str, str]:
                 mapping[result.group(1)] = "id_" + result.group(2)
     return mapping
 
-def process(base_dir: str):
+def process(base_dir: str, index: Optional[str] = None, new_cov_path: Optional[str] = None):
     for dataset in DATASET:
         only_ei_cov_data = set([item[11:] for item in process_cov_data(os.path.join(base_dir, "processed", f"{dataset}-only-ei-fast-cov-all.txt"))])
         for i in range(10):
@@ -25,23 +26,34 @@ def process(base_dir: str):
 
             mapping = build_corpus_map(expreiment_folder)
 
-            for item in sorted(os.listdir(corpus_coverage_folder)):
-                index = item.split(".")[0]
+            if index:
                 if index not in mapping:
                     continue
-
-                cov_data = process_cov_data(os.path.join(corpus_coverage_folder, item))
+                cov_data = process_cov_data(os.path.join(corpus_coverage_folder, index + ".txt"))
                 parent_cov_data = process_cov_data(os.path.join(corpus_coverage_folder, mapping[index] + ".txt"))
                 intersection = cov_data.intersection(only_ei_cov_data) - parent_cov_data
-                if len(intersection) > 100:
-                    print("Index: ", index)
-                    print("Parent: ", mapping[index])
-                    print(len(intersection))
-                    if "5803" in index:
-                        for line in intersection:
-                            print(line)
+                if new_cov_path:
+                    new_cov_data = process_cov_data(new_cov_path)
+                    for item in (intersection - new_cov_data):
+                        print(item)
+            else:
+                for item in sorted(os.listdir(corpus_coverage_folder)):
+                    index = item.split(".")[0]
+                    if index not in mapping:
+                        continue
+
+                    cov_data = process_cov_data(os.path.join(corpus_coverage_folder, item))
+                    parent_cov_data = process_cov_data(os.path.join(corpus_coverage_folder, mapping[index] + ".txt"))
+                    intersection = cov_data.intersection(only_ei_cov_data) - parent_cov_data
+                    if len(intersection) < 100 and len(intersection) > 50:
+                        print("Index: ", index)
+                        print("Parent: ", mapping[index])
+                        print(len(intersection))
 
 
 if __name__ == "__main__":
-    process(sys.argv[1])
+    if len(sys.argv) > 2:
+        process(*sys.argv[1:])
+    else:
+        process(sys.argv[1])
 
