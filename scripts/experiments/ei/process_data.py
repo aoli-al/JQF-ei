66da511b7a73c0c2966e947f81ef7a778b65c3c6
cc-21 <huicong.huang21@gmail.com>
2024-11-24 23:19:10 -0800

    Fix the calculation of effective size

--- scripts/experiments/ei/process_data.py
+++ scripts/experiments/ei/process_data.py
@@ -146,16 +146,18 @@ def generate_perf_graph(data_dirs: List[str], algorithms: Set[str], out_folder: 
         #     out_folder, f"{dataset}-{out_name}.pdf"), corpus_based_plot_data)
 
 
-def mann_whitney_u_test(sample1, sample2, alternative='two-sided', verbose=True):
-    n = len(sample1)
-    m = len(sample2)
-    U1, p = mannwhitneyu(sample1, sample2, alternative=alternative, method='exact')
+def mann_whitney_u_test(treatment, control, alternative='two-sided', verbose=True):
+    m = len(treatment)
+    n = len(control)
+    assert m == n
+
+    U1, p = mannwhitneyu(treatment, control, alternative=alternative, method='exact')
     U2 = n * m - U1
 
     # Effective size: A12
-    rank_results = rankdata(sample1 + sample2)
-    r1 = sum(rank_results[0:n])
-    r2 = sum(rank_results[n:n + m])
+    rank_results = rankdata(treatment + control)
+    r1 = sum(rank_results[0:m])
+    r2 = sum(rank_results[m:m+n])
     # A = (r1/m - (m+1)/2)/n # formula (14) in Vargha and Delaney, 2000
     # equivalent formula to avoid accuracy errors
     A_12 = (2 * r1 - m * (m + 1)) / (2 * n * m)
