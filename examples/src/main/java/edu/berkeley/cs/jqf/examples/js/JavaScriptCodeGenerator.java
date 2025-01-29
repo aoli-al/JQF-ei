a96e95ac3d27b245bb9f5ff50d5955b7b95af130
Ao Li <leeleo3x@gmail.com>
2025-01-29 16:32:21 -0500

    update targets.

--- examples/src/main/java/edu/berkeley/cs/jqf/examples/js/JavaScriptCodeGenerator.java
+++ examples/src/main/java/edu/berkeley/cs/jqf/examples/js/JavaScriptCodeGenerator.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2017-2018 The Regents of the University of California
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package edu.berkeley.cs.jqf.examples.js;
 
 import java.util.ArrayList;
@@ -40,11 +68,17 @@ public class JavaScriptCodeGenerator extends Generator<String> {
     };
 
     private static final String[] BINARY_TOKENS = {
-            "!=", "!==", "%", "%=", "&", "&&", "&=", "*", "*=", "+", "+=", ",",
-            "-", "-=", "/", "/=", "<", "<<", ">>=", "<=", "=", "==", "===",
-            ">", ">=", ">>", ">>=", ">>>", ">>>=", "^", "^=", "|", "|=", "||",
+            "!=", "!==", "%", "&", "&&", "*", "+", ",",
+            "-", "/", "<", "<<", "<=", "=", "==", "===",
+            ">", ">=", ">>", ">>>", "^", "|", "||",
             "in", "instanceof"
     };
+
+    private static final String[] ASSIGNMENT_TOKENS = {
+            "=", "+=", "-=", "*=", "/=", "%=", "**=", "<<=", ">>=", ">>>=", "&=", "^=", "|=", "&&=",
+            "||=", "??=",
+    };
+
 
     @Override
     public String generate(SourceOfRandomness random, GenerationStatus status) {
@@ -89,7 +123,8 @@ public class JavaScriptCodeGenerator extends Generator<String> {
                     this::generateFunctionNode,
                     this::generatePropertyNode,
                     this::generateIndexNode,
-                    this::generateArrowFunctionNode
+                    this::generateArrowFunctionNode,
+                    this::generateAssignmentNode
             )).apply(random);
         }
         expressionDepth--;
@@ -301,5 +336,16 @@ public class JavaScriptCodeGenerator extends Generator<String> {
 
     private String generateWhileNode(SourceOfRandomness random) {
         return "while (" + generateExpression(random) + ")" + generateBlock(random);
+    }
+
+    private String generateAssignmentNode(SourceOfRandomness random) {
+        String token = random.choose(ASSIGNMENT_TOKENS);
+        String lhs = random.choose(Arrays.<Function<SourceOfRandomness, String>>asList(
+                this::generateIdentNode,
+                this::generateIndexNode,
+                this::generatePropertyNode
+        )).apply(random);
+        String rhs = generateExpression(random);
+        return lhs + " " + token + " " + rhs + ";";
     }
 }
--- examples/src/test/java/edu/berkeley/cs/jqf/examples/jython/JythonInterpreterTest.java
+++ examples/src/test/java/edu/berkeley/cs/jqf/examples/jython/JythonInterpreterTest.java
@@ -23,22 +23,15 @@ import org.python.util.PythonInterpreter;
 public class JythonInterpreterTest {
     @Fuzz
     public void testWithGenerator(@From(PythonGenerator.class) String code) throws Throwable {
-        try (PythonInterpreter interp = new PythonInterpreter()) {
-            interp.exec(code);
-        } catch (PyException e) {
-            if (e.getCause() instanceof TimeoutException) {
-                throw e.getCause();
-            }
+        PythonCompiler compiler = new LegacyCompiler();
+        if (code.contains("\0")) {
+            throw Py.TypeError("compile() expected string without null bytes");
         }
-//        PythonCompiler compiler = new LegacyCompiler();
-//        if (code.contains("\0")) {
-//            throw Py.TypeError("compile() expected string without null bytes");
-//        }
-//        CompilerFlags cflags = new CompilerFlags();
-//        CompileMode mode = CompileMode.exec;
-//        String filename = "<String>";
-//        mod node = ParserFacade.parse(code, mode, filename, cflags);
-//        PythonCodeBundle bundle = compiler.compile(node, "org.python.pycode._pyx", filename,
-//                true, true, cflags);
+        CompilerFlags cflags = new CompilerFlags();
+        CompileMode mode = CompileMode.exec;
+        String filename = "<String>";
+        mod node = ParserFacade.parse(code, mode, filename, cflags);
+        PythonCodeBundle bundle = compiler.compile(node, "org.python.pycode._pyx", filename,
+                true, true, cflags);
     }
 }
