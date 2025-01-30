7600f7b9a13e222dbfe042adbadff182964c6453
Ao Li <leeleo3x@gmail.com>
2025-01-30 13:17:39 -0500

    Log execution result.

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/ei/ZestGuidance.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/ei/ZestGuidance.java
@@ -867,7 +867,7 @@ public class ZestGuidance implements Guidance {
         return v0[n];
     }
 
-    private void logMutation(boolean saved, boolean valid) {
+    private void logMutation(boolean saved, Result result) {
         int parametricDistance = -1;
         Input parentInput = savedInputs.get(currentParentInputIdx);
         String parentRaw = parentInput.raw;
@@ -875,7 +875,7 @@ public class ZestGuidance implements Guidance {
             parametricDistance = getLevenshteinDistFromInput(currentInput, parentInput);
             int distance = getLevenshteinDistFromString(currentRaw, parentRaw);
             String text = currentRaw.length() + "," +  parentRaw.length() + "," +
-                    parametricDistance + "," + distance + "," + saved + "," + valid + ","
+                    parametricDistance + "," + distance + "," + saved + "," + result + ","
                     + currentParentInputIdx + ",";
             if (saved) {
                 text += Integer.toString(currentInput.id);
@@ -1008,7 +1008,7 @@ public class ZestGuidance implements Guidance {
             }
 
             if (OBSERVE_MUTATION_DISTANCE && !savedInputs.isEmpty()) {
-                logMutation(toSave, valid);
+                logMutation(toSave, result);
             }
 
 
